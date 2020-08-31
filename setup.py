4c4ac219c7205f3d3422d404bda172ebd60bade2
Leon Oostrum <leonoostrum@gmail.com>
2020-08-31 11:31:56 +0200

    Add initial package structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+
+with open(os.path.join('arts_tracking_beams', '__version__.py')) as version_file:
+    version = {}
+    exec(version_file.read(), version)
+    project_version = version['__version__']
+
+
+setup(name='arts_tracking_beams',
+      version=project_version,
+      description='Create a tracking beam from ARTS tied-array beam data',
+      url='http://github.com/loostrum/arts_tracking_beams',
+      author='Leon Oostrum',
+      author_email='oostrum@astron.nl',
+      license="Apache Software License 2.0",
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=['numpy',
+                        'astropy',
+                        'matplotlib',
+                        'tqdm'],
+      )
